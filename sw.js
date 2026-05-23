158693799742f8e622dec5c7149de9565095ccba
calesimmons-svg <cale.simmons@gmail.com>
2026-05-22 21:25:23 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'flight-time-v3';
+const BASE = '/Atlas-1/';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.add(BASE + 'index.html'))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(BASE + 'index.html'))
+    );
+  }
+});
